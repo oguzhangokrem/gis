0e0027fb5dd79f339fe7f62fea784262fd08288a
Oğuzhan Gökrem <oguzhangokrem@gmail.com>
2025-05-26 22:34:28 +0300

    Modified index.html and added script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+import L, {Map, TileLayer, Marker, Circle, Polygon, Popup, Control} from 'leaflet';
+
+        const map = new Map('map', {
+            zoomControl: false
+        }).setView([41.3442, 36.2494], 8);
+
+        const tiles = new TileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        const zoom = new Control.Zoom({
+            position: 'topleft',
+            zoomInTitle: 'Yaklaş',
+            zoomOutTitle: 'Uzaklaş'
+        }).addTo(map);
+
+        const scale = new Control.Scale({
+            position: 'topright',
+            metric: true,
+            imperial: false
+        }).addTo(map);
